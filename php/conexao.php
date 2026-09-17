7b046e2e6b9bc5d459e1d750d80e93e35ccfe7d6
Isabellecavalcant <isabelle.silva.712@ufrn.edu.br>
2026-09-17 12:04:04 -0300

    feat: permite configurar conexão por variáveis de ambiente

--- php/conexao.php
+++ php/conexao.php
@@ -1,13 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "pnld";
-$password = "123456";
-$dbname = "SistemaHBL";
+
+$servername = getenv("DB_HOST") ?: "localhost";
+$username = getenv("DB_USER") ?: "pnld";
+$password = getenv("DB_PASSWORD") ?: "123456";
+$dbname = getenv("DB_NAME") ?: "SistemaHBL";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
-
-
