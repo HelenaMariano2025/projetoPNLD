41d084bf63d7c4aac7dec0f0fccaead1923e7459
Isabellecavalcant <isabelle.silva.712@ufrn.edu.br>
2026-09-24 22:16:46 -0300

    fix: inclui link para edição na lista de livros #3

--- livros.php
+++ livros.php
@@ -1,70 +1,42 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-  <!-- Basic -->
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <!-- Mobile Metas -->
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <!-- Site Metas -->
-  <meta name="keywords" content="" />
-  <meta name="description" content="" />
-  <meta name="author" content="" />
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <title>Livros Disponíveis</title>
-  <!-- Favicon -->
+
   <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-
-  <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-
-  <!-- Fonts style -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
-
-  <!-- Owl slider stylesheet -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-
-  <!-- Font Awesome style -->
-  <link href="css/font-awesome.min.css" rel="stylesheet" />
-
-  <!-- Nice select -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css"
-    integrity="sha256-mLBIhmBvigTFWPSCtvdu6a76T+3Xyt+K571hupeFLg4=" crossorigin="anonymous" />
-
-  <!-- Datepicker -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.css">
-
-  <!-- Custom styles for this template -->
-  <link href="css/style.css" rel="stylesheet" />
-
-  <!-- Responsive style -->
-  <link href="css/responsive.css" rel="stylesheet" />
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/responsive.css" rel="stylesheet">
 </head>
 
 <body class="sub_page">
   <div class="hero_area">
-    <!-- header section starts -->
     <header class="header_section">
       <div class="header_top">
         <div class="container">
           <div class="contact_nav">
             <a href="">
               <i class="fa fa-phone" aria-hidden="true"></i>
-              <span>
-                Contato : +01 123455678990
-              </span>
+              <span>Contato : +01 123455678990</span>
             </a>
+
             <a href="">
               <i class="fa fa-envelope" aria-hidden="true"></i>
-              <span>
-                Email : ifpb@gmail.com
-              </span>
+              <span>Email : ifpb@gmail.com</span>
             </a>
+
             <a href="">
               <i class="fa fa-map-marker" aria-hidden="true"></i>
-              <span>
-                <a href="https://www.bing.com/maps?osid=2d9cc22c-c352-4b15-84e1-a645eaf97d8a&cp=-7.025562~-37.280592&lvl=17&pi=0&v=2&sV=2&form=S00027">Localização</a>
-              </span>
+              <span>Localização</span>
             </a>
           </div>
         </div>
@@ -72,20 +44,28 @@
 
       <div class="header_bottom">
         <div class="container-fluid">
-          <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <nav class="navbar navbar-expand-lg custom_nav-container">
             <a class="navbar-brand">
               <img src="images/White and navy simple book store logo.png" alt="">
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class=""> </span>
+
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <div class="d-flex mr-auto flex-column flex-lg-row align-items-center">
-                <ul class="navbar-nav  ">
-                  <li class="nav-item ">
-                    <a class="nav-link" href="funcoes.php">HOME <span class="sr-only">(current)</span></a>
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="funcoes.php">HOME</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="add_livro.php">ADICIONAR LIVRO</a>
@@ -97,16 +77,12 @@
         </div>
       </div>
     </header>
-    <!-- end header section -->
   </div>
 
-  <!-- Livros section -->
   <section class="client_section layout_padding">
     <div class="container">
       <div class="heading_container">
-        <h2>
-          <span>Livros</span>
-        </h2>
+        <h2><span>Livros</span></h2>
       </div>
     </div>
 
@@ -114,7 +90,10 @@
       <div id="customCarousel2" class="carousel carousel-fade" data-ride="carousel">
         <div class="carousel-inner">
           <div class="search_container">
-            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8'); ?>" method="get">
+            <form
+              action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8'); ?>"
+              method="get"
+            >
               <input type="text" name="titulo" placeholder="Buscar por título do livro">
               <button type="submit">Buscar</button>
             </form>
@@ -129,31 +108,36 @@
           $result = $repository->consultarDisponiveis($titulo);
 
           if ($result !== false && $result->num_rows > 0) {
-            $count = 0;
+              $count = 0;
 
-            while ($row = $result->fetch_assoc()) {
-              $activeClass = $count === 0 ? 'active' : '';
+              while ($row = $result->fetch_assoc()) {
+                  $activeClass = $count === 0 ? 'active' : '';
 
-              echo "<div class='carousel-item $activeClass'>";
-              echo "<div class='box'>";
-              echo "<div class='client_info'>";
-              echo "<div class='client_name'>";
-              echo '<h5>' . htmlspecialchars((string) $row['isbn'], ENT_QUOTES, 'UTF-8') . '</h5>';
-              echo '<h6>' . htmlspecialchars((string) $row['titulo'], ENT_QUOTES, 'UTF-8') . '</h6>';
-              echo "</div>";
-              echo "</div>";
-              echo '<p>' . htmlspecialchars((string) $row['autor'], ENT_QUOTES, 'UTF-8') . '</p>';
-              echo "</div>";
-              echo "</div>";
+                  echo "<div class='carousel-item $activeClass'>";
+                  echo "<div class='box'>";
+                  echo "<div class='client_info'>";
+                  echo "<div class='client_name'>";
+                  echo '<h5>' . htmlspecialchars((string) $row['isbn'], ENT_QUOTES, 'UTF-8') . '</h5>';
+                  echo '<h6>' . htmlspecialchars((string) $row['titulo'], ENT_QUOTES, 'UTF-8') . '</h6>';
+                  echo '</div>';
+                  echo '</div>';
+                  echo '<p>' . htmlspecialchars((string) $row['autor'], ENT_QUOTES, 'UTF-8') . '</p>';
 
-              $count++;
-            }
+                  echo '<p><a href="editar_livro.php?codigo='
+                      . (int) $row['codigo']
+                      . '">Editar livro</a></p>';
+
+                  echo '</div>';
+                  echo '</div>';
+
+                  $count++;
+              }
           } else {
-            echo "<div class='carousel-item active'>";
-            echo "<div class='box'>";
-            echo "<p>Nenhum livro disponível no momento.</p>";
-            echo "</div>";
-            echo "</div>";
+              echo "<div class='carousel-item active'>";
+              echo "<div class='box'>";
+              echo '<p>Nenhum livro disponível no momento.</p>';
+              echo '</div>';
+              echo '</div>';
           }
 
           $conn->close();
@@ -163,22 +147,21 @@
         <div class="carousel_btn-box">
           <a class="carousel-control-prev" href="#customCarousel2" role="button" data-slide="prev">
             <i class="fa fa-angle-left" aria-hidden="true"></i>
-            <span class="sr-only">Previous</span>
+            <span class="sr-only">Anterior</span>
           </a>
+
           <a class="carousel-control-next" href="#customCarousel2" role="button" data-slide="next">
             <i class="fa fa-angle-right" aria-hidden="true"></i>
-            <span class="sr-only">Next</span>
+            <span class="sr-only">Próximo</span>
           </a>
         </div>
       </div>
     </div>
   </section>
 
-  <!-- footer section -->
   <script src="js/jquery-3.4.1.min.js"></script>
   <script src="js/bootstrap.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js"
-    integrity="sha256-Zr3vByTlMGQhvMfgkQ5BtWRSKBGa2QlspKYJnkjZTmo=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
   <script src="js/custom.js"></script>
